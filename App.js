c4069b28c6777f62fb0ad4621908d9280d92ff51
Adriano Ramos <adriano.ramos3208@gmail.com>
2023-05-30 14:12:45 -0300

    documentação

--- App.js
+++ App.js
@@ -1,5 +1,16 @@
 import RootNavigation from './src';
 
+
+/**
+ * @author Adriano Ramos <adriano.ramos3208@gmail.com>
+ * @since 0.1.0
+ * 
+ * @description app função que
+ * realiza a navegação entre as telas
+ * @param {Object} navigation Objeto de navegação 
+ * @export {Function} App 
+ * @return {React.Component} 
+ */
 export default function App(){
   return (
     <RootNavigation/>
