cfdfca389fe35f36f1b3827c58a7fd0c29dcab02
gadulb <gabrieli.lembeck@alunos.sc.senac.br>
2023-05-31 14:33:40 -0300

    add

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,12 +1,26 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
+/**
+ * Firebase configuration
+ * @author Gabrieli Eduarda Lembeck
+ * 
+ * @const {Object} firebaseConfig Firebase configuration
+ * @const {string} firebaseConfig.apiKey Firebase API key
+ * @const {string} firebaseConfig.authDomain Firebase auth domain
+ * @const {string} firebaseConfig.projectId Firebase project ID
+ * @const {string} firebaseConfig.storageBucket Firebase storage bucket
+ * @const {string} firebaseConfig.messagingSenderId Firebase messaging sender ID
+ * @const {string} firebaseConfig.appId Firebase app ID
+ * @since 0.1.0 
+ */
 const firebaseConfig = {
-  apiKey: "AIzaSyD79zwwA22mOvTAJtqghRuvUK8UHpj9hB4",
-  authDomain: "aquafix-d1c67.firebaseapp.com",
-  projectId: "aquafix-d1c67",
-  storageBucket: "aquafix-d1c67.appspot.com",
-  messagingSenderId: "253279762355",
-  appId: "1:253279762355:web:e2422434169c8e1c1bbdc6"
+  apiKey: "AIzaSyBr1OGo5lOFQ4aTsYQRADiP5WqyDG4zGso",
+  authDomain: "aquafix-4df2e.firebaseapp.com",
+  projectId: "aquafix-4df2e",
+  storageBucket: "aquafix-4df2e.appspot.com",
+  messagingSenderId: "699173900788",
+  appId: "1:699173900788:web:d88401edd38056c75c69a1"
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
