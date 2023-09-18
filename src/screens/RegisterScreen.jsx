ec16a01a6887b4e34504317c8a20134146322aff
LucasCoelho1 <lucas.coelho3@alunos.sc.senac.br>
2023-09-18 16:22:55 -0300

    register att

--- src/screens/RegisterScreen.jsx
+++ src/screens/RegisterScreen.jsx
@@ -64,13 +64,22 @@ export default function RegisterScreen({ navigation }) {
       .replace(/\D/g, "") // Remove non-numeric characters
       .replace(/(\d{2})(\d)/, "($1) $2") // Add parentheses to the area code
       .replace(/(\d{5})(\d)/, "$1-$2") // Add hyphen to the main number
-      .replace(/(\d{4})(\d)/, "$1-$2") // Add hyphen to the main number (for 9-digit numbers)
       .slice(0, 15); // Limit the length of the input
     return formattedValue;
   }
 
+  function formatPhoneNumberCep(value) {
+    // Apply the desired phone number mask here
+    const formattedValueCep = value
+      .replace(/\D/g, "") // Remove non-numeric characters
+      .replace(/(\d{5})(\d)/, "$1-$2") // Add hyphen to the main number
+      .slice(0, 15); // Limit the length of the input
+    return formattedValueCep;
+  }
+
   return (
-    <KeyboardAvoidingView style={styles.container} behavior="padding">
+    // <KeyboardAvoidingView style={styles.container} behavior="padding">
+    <View style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollViewContent}>
         <View style={styles.imagemTopo}>
           <Image
@@ -103,12 +112,14 @@ export default function RegisterScreen({ navigation }) {
               placeholder="Digite seu telefone"
               value={whatsappUsu}
               onChangeText={(value) => setWhatsappUsu(formatPhoneNumber(value))}
+              maxLength={15}
               style={styles.input}
             />
             <TextInput
               placeholder="Digite seu CEP"
               value={zipCode}
-              onChangeText={setZipCode}
+              onChangeText={(e) => setZipCode(formatPhoneNumberCep(e))}
+              maxLength={9}
               style={styles.input}
             />
             <TextInput
@@ -135,6 +146,7 @@ export default function RegisterScreen({ navigation }) {
           </View>
         </View>
       </ScrollView>
-    </KeyboardAvoidingView>
+    </View>
+    // </KeyboardAvoidingView>
   );
 }
