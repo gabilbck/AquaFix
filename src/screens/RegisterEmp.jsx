0f18f46dca9215b6611fed38245990e5a05374f8
gadulb <gabrieli.lembeck@alunos.sc.senac.br>
2023-09-26 16:59:59 -0300

    add

--- src/screens/RegisterEmp.jsx
+++ src/screens/RegisterEmp.jsx
@@ -12,6 +12,9 @@ import { auth, db } from "../config/firebase";
 import { doc, setDoc } from "firebase/firestore";
 import { cnpj } from "cpf-cnpj-validator";
 import { ScrollView } from "react-native-web";
+import { collection, addDoc } from "firebase/firestore";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import * as ImagePicker from "expo-image-picker";
 
 export default function RegisterEmp({ navigation }) {
   const [nomeUsu, setNomeUsu] = useState("");
@@ -28,6 +31,7 @@ export default function RegisterEmp({ navigation }) {
   const [isValid, setIsValid] = useState(null);
   const [erroCnpj, setErroCnpj] = useState("");
   const [servicosUsu, setServicosUsu] = useState("");
+  const [getImage, setImage] = useState(null);
 
   function handleRegister() {
     if (senha !== confirmSenha) {
@@ -46,7 +50,6 @@ export default function RegisterEmp({ navigation }) {
           cep_usu: zipCode,
           cnpj: cnpjUsu,
           email_usu: email,
-          foto_usu: "",
           nome_completo: nomeCompleto,
           nome_usu: nomeUsu,
           senha_usu: senha,
@@ -111,6 +114,53 @@ export default function RegisterEmp({ navigation }) {
 
     console.log(texto);
   }
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+        setImage(result.assets[0].uri);
+    }
+};
+
+const uploadImageToFirebase = async () => {
+  try {
+      const response = await fetch(getImage);
+      const blob = await response.blob();
+
+      const storageRef = ref(storage, 'foto_usu/' + Date.now())
+      ;
+      const uploadTask = uploadBytes(storageRef, blob);
+
+      await uploadTask;
+
+      const imageURL = await getDownloadURL(storageRef);
+      setImageToFirebase(imageURL);
+  } catch (error) {
+      console.error('Error uploading image: ', error);
+  }
+};
+
+const setImageToFirebase = async (url) => {
+  try {
+      const ref = collection(db, 'foto_usu');
+      await addDoc(ref, { url });
+
+      console.log('URL da imagem enviada a Firestore');
+      setImage(null);
+  } catch (error) {
+      console.error('Erro ao enviar a Firestore: ', error);
+      setImage(null);
+  }
+};
+
 
   return (
     <ScrollView>
@@ -189,6 +239,14 @@ export default function RegisterEmp({ navigation }) {
               onChangeText={setServicosUsu}
               style={styles.input}
             />
+            {getImage ? <> 
+              <Image source={{ uri: getImage }} style={{ width: 200, height: 200, borderRadius: "50%", alignSelf: "center", marginTop: 10, marginBottom: 10, border: "4px #16337E solid"}} />
+              </>
+              :
+              <Button onPress={pickImage} style={styles.botao} textColor="white">
+                Escolher foto
+              </Button>
+            }
 
             <Button
               onPress={handleRegister}
