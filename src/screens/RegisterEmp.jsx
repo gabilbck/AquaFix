40f8a771e73c37c6648b780bc83987f63f8d69f0
gadulb <gabrieli.lembeck@alunos.sc.senac.br>
2023-09-27 14:46:24 -0300

    add

--- src/screens/RegisterEmp.jsx
+++ src/screens/RegisterEmp.jsx
@@ -32,22 +32,12 @@ export default function RegisterEmp({ navigation }) {
   const [servicosUsu, setServicosUsu] = useState("");
   const [getImage, setImage] = useState(null);
   const [erroSenha, setErroSenha] = useState("");
-  const [imageSelected, setImageSelected] = useState(false);
-  const [erroImage, setErroImage] = useState("");
 
   function handleRegister() {
     if (senha !== confirmSenha) {
       setErroSenha("As senhas não correspondem");
       return;
     }
-
-    if (!imageSelected) {
-    // Se a imagem não foi selecionada, exiba uma mensagem de erro ou tome a ação apropriada.
-    // Por exemplo, você pode definir um estado de erro para exibir uma mensagem de erro na interface do usuário.
-    // Ou você pode impedir o envio do formulário até que a imagem seja selecionada.
-    setErroImage("Selecione uma imagem antes de cadastrar.");
-    return;
-  }
 
     createUserWithEmailAndPassword(auth, email, senha)
       .then((userCredential) => {
@@ -127,47 +117,50 @@ export default function RegisterEmp({ navigation }) {
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
     });
-  
-    if (!result.canceled) {
-      setImage(result.assets[0].uri);
-      setImageSelected(true); // Defina como true quando uma imagem for selecionada
-    }
-  };
 
-  const uploadImageToFirebase = async () => {
-    try {
+    console.log(result);
+
+    if (!result.canceled) {
+        setImage(result.assets[0].uri);
+    }
+};
+
+const uploadImageToFirebase = async () => {
+  try {
       const response = await fetch(getImage);
       const blob = await response.blob();
 
-      const storageRef = ref(storage, "foto_usu/" + Date.now());
+      const storageRef = ref(storage, 'foto_usu/' + Date.now())
+      ;
       const uploadTask = uploadBytes(storageRef, blob);
 
       await uploadTask;
 
       const imageURL = await getDownloadURL(storageRef);
       setImageToFirebase(imageURL);
-    } catch (error) {
-      console.error("Error uploading image: ", error);
-    }
-  };
+  } catch (error) {
+      console.error('Error uploading image: ', error);
+  }
+};
 
-  const setImageToFirebase = async (url) => {
-    try {
-      const ref = collection(db, "foto_usu");
+const setImageToFirebase = async (url) => {
+  try {
+      const ref = collection(db, 'foto_usu');
       await addDoc(ref, { url });
 
-      console.log("URL da imagem enviada a Firestore");
+      console.log('URL da imagem enviada a Firestore');
       setImage(null);
-    } catch (error) {
-      console.error("Erro ao enviar a Firestore: ", error);
+  } catch (error) {
+      console.error('Erro ao enviar a Firestore: ', error);
       setImage(null);
-    }
-  };
+  }
+};
+
 
   return (
     <ScrollView>
@@ -247,31 +240,14 @@ export default function RegisterEmp({ navigation }) {
               onChangeText={setServicosUsu}
               style={styles.input}
             />
-            {getImage ? (
-              <>
-                <Image
-                  source={{ uri: getImage }}
-                  style={{
-                    width: 200,
-                    height: 200,
-                    borderRadius: "50%",
-                    alignSelf: "center",
-                    marginTop: 10,
-                    marginBottom: 10,
-                    border: "4px #16337E solid",
-                  }}
-                />
+            {getImage ? <> 
+              <Image source={{ uri: getImage }} style={{ width: 200, height: 200, borderRadius: "50%", alignSelf: "center", marginTop: 10, marginBottom: 10, border: "4px #16337E solid"}} />
               </>
-            ) : (
-              <Button
-                onPress={pickImage}
-                style={styles.botao2}
-                textColor="white"
-              >
+              :
+              <Button onPress={pickImage} style={styles.botao2} textColor="white">
                 Escolher foto
               </Button>
-            )}
-            <Text>{erroImage}</Text>
+            }
 
             <Button
               onPress={handleRegister}
@@ -287,9 +263,7 @@ export default function RegisterEmp({ navigation }) {
                   textColor={"black"}
                   onPress={() => navigation.navigate("CadPasso2")}
                 >
-                  <Text style={styles.botaoPreto}>
-                    Voltar ao passo anterior
-                  </Text>
+                  <Text style={styles.botaoPreto}>Voltar ao passo anterior</Text>
                 </Button>
               </View>
               <View style={styles.coluna}>
