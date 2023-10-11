e2928ef119f70f6a45db22a6761af91cd2fbbec1
Amanda <eunumseioqpordeemail@gmail.com>
2023-10-11 16:50:58 -0300

    aaaaaaaaaaaaaaaaaaaa

--- src/screens/EditUsu.jsx
+++ src/screens/EditUsu.jsx
@@ -224,21 +224,3 @@ export default function EditProfile({ navigation }) {
     </View>
   );
 }
-
-/* 
-<View style={styles.container}>
-      {/* Parte que aparece a imagem: azul e logo }
-      <View style={styles.imagemTopo}>
-        <Image
-          source={require("../../assets/img/logocomp-branca.png")}
-          style={{ width: 200, height: 127 }}
-        />
-      </View>
-      {/* Parte que aparece o conteúdo: cinza/branco }
-      <View style={styles.conteudo}>
-        <View style={styles.containerInner}>
-          {/* CONTEÚDO }
-        </View>
-      </View>
-    </View>
-*/
