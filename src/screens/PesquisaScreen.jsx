ed327730b6d945c05afe1996f8f682c7284aba05
gadulb <gabrieli.lembeck@alunos.sc.senac.br>
2023-10-18 16:11:13 -0300

    add

--- src/screens/PesquisaScreen.jsx
+++ src/screens/PesquisaScreen.jsx
@@ -1,65 +1,74 @@
 import React, { useState, useEffect } from "react";
+import { View, TextInput, Text, FlatList } from "react-native";
 import {
-  View,
-  TextInput,
-  Button,
-  Text,
-  StyleSheet,
-  FlatList,
-} from "react-native";
-import {
-  startAt,
-  endAt,
   collection,
   query,
   where,
   getDocs,
+  startAt,
+  endAt,
   orderBy,
-  doc,
-  getFirestore,
 } from "firebase/firestore";
-import { app, db } from "../config/firebase";
+import { db } from "../config/firebase";
 
 import { styles } from "../utils/styles";
 import { Image } from "expo-image";
+import { Button, Card } from "react-native-paper";
 
 export default function PesquisaScreen({ navigation }) {
-  const [resultadoPesquisa, setResultadoPesquisa] = useState([]);
-  const [busca, setBusca] = useState("");
   const [resultado, setResultado] = useState([]);
+  const [busca, setBusca] = useState("");
 
   async function buscarServico() {
+    if (busca.trim() === "") {
+      // Não faça a consulta se o campo de busca estiver vazio
+      setResultado([]);
+      return;
+    }
+
     const usuarioRef = collection(db, "usuario");
     const buscaServico = query(
       usuarioRef,
-      where("servicos_usu", "==", busca),
-      where("servicos_usu1", "==", busca),
-      where("servicos_usu2", "==", busca),
+      where("servicos_usu", ">=", busca),
       orderBy("servicos_usu"),
-      startAt(busca + "\uf8ff"),
+      startAt(busca),
       endAt(busca + "\uf8ff")
     );
+
     const resultadoSnapshot = await getDocs(buscaServico);
     const listaUsuarios = resultadoSnapshot.docs.map((doc) => doc.data());
-    console.log(listaUsuarios);
-    setResultado(listaUsuarios);
 
-    // const usuarioRef = collection(db, "usuario");
-    // const buscaServico = query(
-    //   usuarioRef,
-    //   orderBy("servicos_usu"),
-    //   startAt(palavraChave),
-    //   endAt(palavraChave + "\uf8ff")
-    // );
+    // Agora, adicione a consulta para "servicos_usu1" e "servicos_usu2"
+    const buscaServico1 = query(
+      usuarioRef,
+      where("servicos_usu1", ">=", busca),
+      orderBy("servicos_usu1"),
+      startAt(busca),
+      endAt(busca + "\uf8ff")
+    );
 
-    // const resultadoSnapshot = await getDocs(buscaServico);
+    const resultadoSnapshot1 = await getDocs(buscaServico1);
+    const listaUsuarios1 = resultadoSnapshot1.docs.map((doc) => doc.data());
 
-    // const listaServicos = resultadoSnapshot.docs.map((doc) => doc.data());
-    // console.log(listaServicos);
-    // setResultadoPesquisa(listaServicos);
-    // if (palavraChave === "") {
-    //   setResultadoPesquisa([]);
-    // }
+    const buscaServico2 = query(
+      usuarioRef,
+      where("servicos_usu2", ">=", busca),
+      orderBy("servicos_usu2"),
+      startAt(busca),
+      endAt(busca + "\uf8ff")
+    );
+
+    const resultadoSnapshot2 = await getDocs(buscaServico2);
+    const listaUsuarios2 = resultadoSnapshot2.docs.map((doc) => doc.data());
+
+    // Combine todas as listas de resultados
+    const resultadoFinal = [
+      ...listaUsuarios,
+      ...listaUsuarios1,
+      ...listaUsuarios2,
+    ];
+
+    setResultado(resultadoFinal);
   }
 
   useEffect(() => {
@@ -68,33 +77,52 @@ export default function PesquisaScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      {/* Parte que aparece a imagem: azul e logo */}
       <View style={styles.imagemTopo}>
         <Image
           source={require("../../assets/img/logocomp-branca.png")}
           style={{ width: 200, height: 127 }}
         />
       </View>
-      {/* Parte que aparece o conteúdo: cinza/branco */}
       <View style={styles.conteudo}>
         <View style={styles.containerInner}>
-          {/* CONTEÚDO */}
           <TextInput
             placeholder="Faça sua busca"
             value={busca}
             onChangeText={setBusca}
             style={styles.input}
           />
-          <FlatList
-            data={resultado}
-            renderItem={({ item }) => (
-              <View>
-                <Text>{item.nome_usu}</Text>
-                <Text>{item.servicos_usu}</Text>
-                <Text>{item.bio_usu}</Text>
-              </View>
-            )}
-          />
+          {resultado.length > 0 && (
+            <FlatList
+              data={resultado}
+              renderItem={({ item }) => (
+                <View>
+                  <Card>
+                    <Card.Title
+                      title={item.nome_usu}
+                      fontWeight="bold"
+                      style={{
+                        fontSize: 25,
+                        fontWeight: "bold",
+                      }}
+                    />
+                    <Card.Content>
+                      <Text>{item.servicos_usu}</Text>
+                      <Text>{item.servicos_usu1}</Text>
+                      <Text>{item.servicos_usu2}</Text>
+                    </Card.Content>
+                    <Card.Actions>
+                      <Button
+                        style={styles.cardButton}
+                        onPress={() => Linking.openURL(publicacao.link)}
+                      >
+                        Acessar
+                      </Button>
+                    </Card.Actions>
+                  </Card>
+                </View>
+              )}
+            />
+          )}
         </View>
       </View>
     </View>
